0c4301e00a20f1db710807d4b1e283bf0a954689
rootel <97437816+rootel123@users.noreply.github.com>
2024-04-10 15:41:57 +0900

    Update 배열 만들기 2.js

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -5,6 +5,11 @@ function solution(l, r) {
         if(![...arr].every(x=>x==='5'||x==='0'))continue;
         answer.push(i)
     }
+
+    //이 배열에 만약 5나 0이 아닌 배열을 들어가 있으며 continue로 넘어가고
+    // 아니면 answer에 넣어준다. 
+    //every() 모든 원소가 조건에 맞는지 검사하는 메서드
+    //모든 원소가 조건을 만족하면 true, 하나라도 만족하지 않으면 false를 반환합니다.
     
     return answer.length? answer:[-1];
 }
